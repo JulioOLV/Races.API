5657042f0be1243294f1faefd3e22f2c65f5a50f
JulioOLV <julioccolv@gmail.com>
2023-01-21 11:52:13 -0300

    feat: adiciona estrutura do projeto

---
+++ races-service/src/main/java/ddg/races/api/grpc/GrpcServer.java
@@ -0,0 +1,13 @@
+package ddg.races.api.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcServer.class, args);
+	}
+
+}
---
+++ races-service/src/main/java/ddg/races/api/grpc/service/RaceServiceImpl.java
@@ -0,0 +1,30 @@
+package ddg.races.api.grpc.service;
+
+import ddg.races.Race;
+import ddg.races.RaceId;
+import ddg.races.RaceServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class RaceServiceImpl extends RaceServiceGrpc.RaceServiceImplBase {
+    @Override
+    public void find(RaceId request, StreamObserver<Race> responseObserver) {
+        String id = request.getId();
+        System.out.println("Received Message: " + id);
+
+        Race response = Race.newBuilder()
+                .setMaxAge(150)
+                .setTrend(". A maioria dos anões é leal, pois acreditam\n" +
+                        "firmemente nos benefícios de uma sociedade bem\n" +
+                        "organizada. Eles tendem para o bem, com um forte senso\n" +
+                        "de honestidade e uma crença de que todos merecem\n" +
+                        "compartilhar os benefícios de uma ordem social justa.")
+                .setHeight(1.5)
+                .setDisplacement(9.0)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
